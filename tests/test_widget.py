12e1aa8a5b7f4b43f021a8a293a50c2d6d516fba
Sergey <sergey199433@gmail.com>
2024-05-16 00:23:45 +0400

    create test_masks.py

---
+++ tests/test_widget.py
@@ -0,0 +1,55 @@
+import pytest
+
+from src.widget import get_new_data, mask_account_card
+
+
+@pytest.fixture
+def number_string():
+    return [
+        "Maestro 1596 83** **** 5199",
+        "**9589",
+        "MasterCard 7158 30** **** 6758",
+        "**5560",
+        "Visa Classic 6831 98** **** 7658",
+        "Visa Platinum 8990 92** **** 5229",
+        "Visa Gold 5999 41** **** 6353",
+        "**4305",
+    ]
+
+
+@pytest.mark.parametrize(
+    "number_string, result",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
+def test_mask_account_card(number_string, result):
+    assert mask_account_card(number_string) == result
+
+
+@pytest.fixture
+def old_data():
+    return [
+        "11.07.2018",
+        "03.07.2019",
+        "30.06.2018",
+    ]
+
+
+@pytest.mark.parametrize(
+    "old_data, result",
+    [
+        ("2018-07-11T02:26:18.671407", "11.07.2018"),
+        ("2019-07-03T18:35:29.512364", "03.07.2019"),
+        ("2018-06-30T02:08:58.4255722", "30.06.2018"),
+    ],
+)
+def test_get_new_data(old_data, result):
+    assert get_new_data(old_data) == result
